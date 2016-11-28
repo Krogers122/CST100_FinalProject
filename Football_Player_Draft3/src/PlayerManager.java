ceb24ae0741cfc0020026eda394c506b3f6f284a
Krogers122 <kevinrog122@gmail.com>
2016-11-27 21:44:02 -0700

    Add files via upload

---
+++ Football_Player_Draft3/src/PlayerManager.java
@@ -0,0 +1,12 @@
+/*Week 3 NFL Player assignment
+ * Create a PlayerManager class and create an array of players.  
+ * Create a class method called CreatPlayers
+ */
+public class PlayerManager {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
