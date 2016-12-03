43a666e7b344c3e7cc74ca24a8e762b529ae5712
Krogers122 <kevinrog122@gmail.com>
2016-12-03 11:09:41 -0700

    Update PlayerManager.java

--- Football_Player_Draft3/src/PlayerManager.java
+++ Football_Player_Draft3/src/PlayerManager.java
@@ -1,35 +1,16 @@
-/*
-Football Draft Picker
-Kevin Rogers
-CST-100
-November 20, 2016
-Dr. Mark Lively
+/*Week 3 NFL Player assignment
+ * Create a PlayerManager class and create an array of players.  
+ * Create a class method called CreatPlayers
+ */
 
-Create the NFLPlayer class and add its properties.
-Include player statistics found on the NFL website.
-Use the variable types learned so far to create the player properties.
-*/
+public class PlayerManager extends NFLPlayer {
 
-public class playerClass extends NFLPlayer{
-
-	public playerClass(){
-				
+public static void main(String[] args) {
+	
+	public class createPlayers[] {
+		createPlayers = 
+		System.out.println("Input your new player");
+		
 	}
-	
-	public static void playerClass() {
-	System.out.println("Players list will appear hear");
-	NFLPlayer drewBrees = new playerClass();
-	NFLPlayer kirkCousins = new playerClass();
-	
-	NFLPlayer [] playerList = new NFLPlayer [20];
-	
-	playerList[0] = drewBrees;
-	playerList[1] = kirkCousins;
-	
-	System.out.println("Your favorite player is " + playerList[0]);
-	
-	
-	
-	}	
-
+}
 }
